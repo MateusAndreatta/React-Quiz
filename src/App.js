088fa6c3cef7dfe0064cb06fc63285dd84a9359a
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-09-17 17:25:31 -0300

    implementação de navegação

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route,BrowserRouter} from 'react-router-dom'
+
 import Inicio from './Home/Inicio';
 import Categorias from './Jogo/Categorias';
 import Perguntas from './Jogo/Perguntas';
 import Resultado from './Jogo/Resultado';
-import Ranking from './Jogo/Ranking'
+import Ranking from './Jogo/Ranking';
+import Header from './Jogo/Header';
+
+
+//<img src={logo} className="App-logo" alt="logo" />
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Ranking/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Header/>
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Route path='/' exact component={Inicio}/>
+          <Route path='/categorias' component={Categorias}/>
+          <Route path='/perguntas' component={Perguntas}/>
+          <Route path='/resultado' component={Resultado}/>
+          <Route path='/ranking' component={Ranking}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
