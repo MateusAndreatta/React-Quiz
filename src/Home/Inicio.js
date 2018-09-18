b0e5afe5281986b50164da0a93828d48257f0e2a
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-09-18 17:36:18 -0300

    firebase e login com fb

--- src/Home/Inicio.js
+++ src/Home/Inicio.js
@@ -1,32 +1,80 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Container, Card, Button, Header, Segment} from 'semantic-ui-react'
+import {Redirect} from 'react-router-dom'
 
 import Navegacao from './Navegacao'
+import config, {auth, providers} from './../config'
 
-const Inicio = props =>{
-    return(
-        <div>
-            <Navegacao/>
-            <br></br>
-            <Container>
-                <Segment piled>Quiz</Segment>
-                <Header as='h2'>Jogo de perguntas e respostas</Header>
-                <p>Desafie seus amigos nesse jogo!</p>
-                
-                <Card centered> 
-                    <Card.Content>
-                        Acesse agora mesmo
-                    </Card.Content>
-                    <Card.Content>
-                        <Button color='facebook'>Login com Facebook</Button>
-                        <Button color='twitter'>Login com Twitter</Button>
-                        <Button basic color='blue'>Usuario Admin</Button>
-                        
-                    </Card.Content>     
-                </Card>
-            </Container>
-        </div>
-    )
+class Inicio extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            usuario:{},
+            estaLogado: false
+        }
+
+        auth.onAuthStateChanged((usuario) => {
+            if(usuario){
+                console.log(usuario)
+                this.setState({
+                    usuario,
+                    estaLogado: true
+                })
+                localStorage.setItem('nome',usuario.displayName)
+                localStorage.setItem('foto',usuario.photoURL)
+            } else{
+                console.log("nao logou")
+                this.setState({
+                    estaLogado: false
+                })
+            }
+        })
+    }
+
+    autentica(provider){
+        auth.signInWithPopup(providers[provider])
+    }
+
+    render(){
+        if(this.state.estaLogado){
+            return <Redirect to='/categorias'/>
+        }
+        return(
+            <div>
+                <Navegacao/>
+                <br></br>
+                <Container>
+                    <Segment piled>Quiz</Segment>
+                    <Header as='h2'>Jogo de perguntas e respostas</Header>
+                    <p>Desafie seus amigos nesse jogo!</p>
+                    {
+                        !this.state.estaLogado &&
+                    <Card centered> 
+                        <Card.Content>
+                            Acesse agora mesmo
+                        </Card.Content>
+                        <Card.Content>
+                            <Button color='facebook' onClick={() => this.autentica('facebook')}>Login com Facebook</Button>
+                            <Button color='twitter'>Login com Twitter</Button>
+                            <Button basic color='blue'>Usuario Admin</Button>
+                            
+                        </Card.Content>     
+                    </Card>
+                    }
+                    {
+                        this.state.estaLogado &&
+                        <div>
+                            <h3>{this.state.usuario.displayName}</h3>
+                            <img src={this.state.usuario.photoURL}/>
+                        </div>
+
+                    }
+                </Container>
+            </div>
+        )
+    }
 }
 
 export default Inicio
---
+++ src/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+import Rebase from 're-base'
+
+var firebaseInfo = firebase.initializeApp({
+    apiKey: "AIzaSyBqIrdcYhzO8-_W-badwQGXC_O5hZACcW8",
+    authDomain: "quizreact.firebaseapp.com",
+    databaseURL: "https://quizreact.firebaseio.com",
+    projectId: "quizreact",
+    storageBucket: "quizreact.appspot.com",
+    messagingSenderId: "706298678236"
+});
+const db = firebase.database(firebaseInfo)
+const config = Rebase.createClass(db)
+
+export const providers = {
+    'facebook': new firebase.auth.FacebookAuthProvider,
+}
+
+export const auth = firebaseInfo.auth()
+export default config
