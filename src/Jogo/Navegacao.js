bdd398ee0efc25e5e84cc69f8b4791dada32fbef
Andreatta <30483277+MateusAndreatta@users.noreply.github.com>
2018-09-18 18:08:48 -0300

    deslogar usuario

--- src/Jogo/Navegacao.js
+++ src/Jogo/Navegacao.js
@@ -1,28 +1,101 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Menu,Image} from 'semantic-ui-react'
+import {Menu,Image, Dropdown,Icon} from 'semantic-ui-react'
+import firebase from 'firebase'
 
-const Navegacao = props =>{
-    const nome = localStorage.getItem('nome')
-    const foto = localStorage.getItem('foto')
-    return(
-        <div>
-            <header className="App-header">
-                <h1 className="App-title">Jogo de perguntas e respostas</h1>
+
+class Navegacao extends Component {
+
+    constructor(props){
+        super(props)
+    
+        this.state = {
+            usuario: '',
+            estaLogado: false
+        }
+    }
+
+    componentDidMount(){
+        
+        const usuarioAtual = firebase.auth().currentUser
+
+        if(usuarioAtual !== null){
+
+            const usuarioLogado = {
+                nome: usuarioAtual.displayName,
+                foto: usuarioAtual.photoURL
+            }
+    
+            this.setState({
+                usuario: usuarioLogado,
+                estaLogado: true
+            })
+
+        }
+
+    }
+
+    deslogarUsuario(){
+        firebase.auth().signOut()
+        .then(() =>{
+            this.setState({
+                usuario: '',
+                estaLogado: false
+            })
+        })
+        .catch(err => {
+            console.log('Erro ao deslogar o usuario')
+        })
+    }
+    
+    render(){
+        if(!this.state.estaLogado){
+            return(
                 <Menu>
                     <Menu.Item><strong>Quiz</strong></Menu.Item>
-                    <Menu.Item as={Link} to='/'>Home</Menu.Item>
-                    <Menu.Item as={Link} to='/categorias'>Categorias</Menu.Item>
-                    <Menu.Item as={Link} to='/perguntas'>Perguntas</Menu.Item>
-                    <Menu.Item as={Link} to='/resultado'>Resultado</Menu.Item>
-                    <Menu.Item as={Link} to='/ranking'>Ranking</Menu.Item>
-                    <Menu.Menu position='right'>
-                        <Menu.Item><Image avatar src={foto}/>{nome}</Menu.Item>
-                    </Menu.Menu>
+                    <Menu.Item as={Link} to='/'>Para jogar, realize o login</Menu.Item>
                 </Menu>
-            </header>
-        </div>
-    )
+            )
+        }
+        return(
+            <div>
+                <header className="App-header">
+                    <h1 className="App-title">Jogo de perguntas e respostas</h1>
+                    <Menu>
+                        <Menu.Item><strong>Quiz</strong></Menu.Item>
+                        <Menu.Item as={Link} to='/'>Home</Menu.Item>
+                        <Menu.Item as={Link} to='/categorias'>Categorias</Menu.Item>
+                        <Menu.Item as={Link} to='/perguntas'>Perguntas</Menu.Item>
+                        <Menu.Item as={Link} to='/resultado'>Resultado</Menu.Item>
+                        <Menu.Item as={Link} to='/ranking'>Ranking</Menu.Item>
+                        <Menu.Menu position='right'>
+
+                        {
+                            this.state.estaLogado &&
+                                <Menu.Item><Image avatar src={this.state.usuario.foto}/></Menu.Item>
+                        }
+                        {
+                            this.state.estaLogado &&
+                            <Dropdown item text={this.state.usuario.nome} >
+                                <Dropdown.Menu>
+                                    <Dropdown.Item onClick={this.deslogarUsuario}>Sair</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        }
+
+                        {
+                            !this.state.estaLogado &&
+                            <Menu.Item><Icon name='user' /></Menu.Item>
+                        }
+
+                        </Menu.Menu>
+
+                    </Menu>
+                </header>
+            </div>
+        )
+    }
+    
 }
 
 export default Navegacao
